11fbaff08852200347d937678204288e6e1bbc84
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2022-01-13 17:21:16 -0500

    Add project files.

---
+++ StackConsole/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackConsole
+{
+    class Stack
+    {
+        public List<object> contents = new List<object>();
+
+
+        public void Push(object obj)
+        {
+            contents.Add(obj);
+        }
+        public object Pop()
+        {
+            Object value;
+            value = contents[contents.Count - 1];
+            contents.RemoveAt(contents.Count - 1);
+            return value;
+        }
+        public void Clear()
+        {
+            contents.Clear();
+        }
+    }
+}
